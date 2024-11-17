8ff985325764735a31eedb0bc2df1eeca9d3a79f
Sandu Davide Filippo <sandudavidefilippo@gmail.com>
2024-11-17 19:38:37 +0100

    Add files via upload

--- Esercizi/Cifrari/soluzione.c
+++ Esercizi/Cifrari/soluzione.c
@@ -4,26 +4,30 @@ void letterLower(char stringa[], int index); //Controlla tutti i caratteri prese
 void isLetter(char stringa[], int index); //Controlla tutti i caratteri presenti in stringa[] e' toglie tutti i caratteri diversi dalle lettere minuscole
 int selectionCifrario(); //Permette l'inserimento di un singolo valore tra (V,v | P,p) e' ritorna il valore della selezione
 
-typedef enum {A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z} alphabet;
+typedef enum {A=61,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z} alphabet;
 typedef enum{Vigenere=1,Playfair} typeCifrario;
 
 void cifrarioVigenere(char stringa[], int index);
 void cifrarioPlayfair(char stringa[], int index);
 
-void eliminazioneInput(char stringa[], int sizeLength, const int lengthOfString); //Elimina i caratteri non utilizzati che rimangono nella memoria dell'input
-int lengthString(const char string[], const int index); //Controlla la lunghezza effettiva della stringa dopo la lavorazione
+void deleteInput(char stringa[], int sizeLength, const int lengthOfString); //Elimina i caratteri non utilizzati che rimangono nella memoria dell'input
+int lengthString(const char string[]); //Controlla la lunghezza effettiva della stringa dopo la lavorazione
 void inputString(char stringa[], int index); //Permette l'inserimento di caratteri grazie alla funzione fgets, controlla la sua lunghezza effettiva e' invoca eliminazioneInput
 
 void puliziaScan(); //Pulisce la memoria dell'input se presente un '\n'
+
+const int lastIndex = 1;
+
+void repeatKey(char key[], int lengthKey, char newKey[], int lengthNewKey); //Ripete la chiave inserita fino alla lunghezza effettiva del plaintext
+void cypherOfText(char stringa[],char key[], char cypherText[], int lengthCypher); //Cifra il il testo utilizzando il cifrario di Vigenere
 
 int main(){
     puts("-------------------------------------------------------------");
     puts("-- CIFRARIO");
     puts("-------------------------------------------------------------");
-    const int indexString = 24;
+    const int indexString = 24+lastIndex;
     char stringa[indexString];
     printf("Inserisci il testo in chiaro: ");
-    
     inputString(stringa, indexString);
 
     printf("TESTO INSERITO: %s\n",stringa);
@@ -51,10 +55,10 @@ int main(){
 
 void letterLower(char stringa[], int index){
     /*  essendo che char stringa[] e' un puntatore esso accedera'
-        all'indirizzo di memoria della stringa passata e' cambiare il suo valore */
-    
+        all'indirizzo di memoria della "stringa" passata e' cambiare il suo valore */
     for(int i=0; i < index; i++){
-        // 'A'=65, 'Z'=90
+        // 'A'=65, 'Z'=90, 'a'=97, 'z'=122
+        //aggiungo 32 al valore del carattere così da avere la stessa lettere minuscola (32 differenza tra 'A' e' 'a')
         if(stringa[i] >= 65 && stringa[i] <= 90){
             stringa[i] += 32;
         }
@@ -63,21 +67,24 @@ void letterLower(char stringa[], int index){
 
 void isLetter(char stringa[], int index){
     char changeString[index];
-    int j=0;
+    int j=0; //questa variabile tiene traccia della posizione attuale del puntatore changeString, aumento il suo valore se trovo un carattere valido
     for(int i=0; i < index; i++){
+        // '\0' e' il carattere di terminazione, se trovo tale carattere significa che ho controllato tutti
+        // i caratteri che mi interessavano dunque fermo il ciclo così da non analizzare piu' niente
         if (stringa[i] == '\0') {
             break;
         }
-
+        //visto che lavoro con i char potevo utilizzare al posto di 97 e 122: 'a', 'z'
         if(stringa[i] >= 97 && stringa[i] <= 122){
             changeString[j] = stringa[i];
             j++;
         }
         
     }
-    changeString[j] = '\0';
+    changeString[j] = '\0'; //Inserisco il carattere di terminazione all'ultima posizione cosi' se stampero' (%s, changeString) visualizzero' tutti i caratteri correttamente
 
     for(int i=0; i <= j; i++){
+        //entrambi gli array hanno la stessa grandezza quindi posso cambiare i valori dell'array stringa con quelli che mi interessano (cosi' succesivamente posso utilizzare sempre "stringa")
         stringa[i] = changeString[i];
     }
 
@@ -87,11 +94,11 @@ int selectionCifrario(){
     puts("\n-------------------------------------------------------------");
     puts("-- SCELTA DEL CIFRARIO");
     puts("-------------------------------------------------------------");
-    char key=0;
-    int option=0;
+    char key=0; //andra' a contenre il carattere preso in input dall'utente
+    int option=0; //andra' a contere il valore della scelta dell'utente per poi essere utilizzata con typeCifrario
 
     do{
-    puts("Scegli il cifrario che vuoi utilizzare inserendo uno \ndi questi valori, (V,v) Vigenere, (P,p) Playfair");
+    puts("Scegli il cifrario che vuoi utilizzare: \n1)Inserisci uno di questi valori (V,v) per usare Vigenere,\n2)Inserisci uno di questi valori (P,p) per usare Playfair ");
     printf("Inserisci il valore: ");
     scanf("%c", &key);
     
@@ -100,11 +107,14 @@ int selectionCifrario(){
     }else if(key == 'P' || key == 'p'){
         option=2;
     }else{
-        puts("Errore non hai inserito un carattere valido!");
+        puts("\n---------------------------------------------");
+        puts("ERRORE: non hai inserito un carattere valido!");
+        puts("---------------------------------------------");
         option=0;
     }
     }while (option==0);
 
+    //uso puliziaScan per togliere l'ultimo \n rimasto nel buffer dell'input
     puliziaScan();
     return option;
 }
@@ -114,16 +124,29 @@ void cifrarioVigenere(char stringa[], int index){
     puts("-- CIFRARIO DI VIGENERE");
     puts("-------------------------------------------------------------");
 
-    alphabet a=0;
-    const int keylength=8;
-    char keyString[8];
+    const int keylength=8+lastIndex;
+    char keyString[keylength]; //chiave preso in input
+
+    char newKey[index]; //questo array conterra' la chiave inserita dall'utente della lunghezza del testo da cifrare e senza caratteri al di fuori delle lettere minuscole
     
     printf("Inserisci la chiave: ");
     inputString(keyString, keylength);
-
+    
     letterLower(keyString, keylength);
     isLetter(keyString, keylength);
-    printf("CHIAVE: %s", keyString);
+    printf("CHIAVE: %s\n", keyString);
+
+    int lengthOfText = lengthString(stringa); //Mi serve la lunghezza effettiva dell'array
+    int lengthOfKey = lengthString(keyString);
+    repeatKey(keyString, lengthOfKey, newKey, lengthOfText);
+
+    printf("Testo:  %s\n", stringa);
+    printf("CHIAVE: %s\n", newKey);
+    
+    char cypherText[lengthOfText]; //conterra' grazie alla funzione cypherOfText() il testo criptato
+    cypherOfText(stringa,newKey,cypherText,lengthOfText);
+
+    printf("RISULT: %s\n", cypherText);
     
 }
 
@@ -133,15 +156,16 @@ void cifrarioPlayfair(char stringa[], int index){
     puts("-------------------------------------------------------------");
 }
 
-void eliminazioneInput(char stringa[], int sizeLength, const int lengthOfString){
-    puts("Entrato Eliminato");
+void deleteInput(char stringa[], int sizeLength, const int lengthOfString){
+    /*usiamo questa funzione quando abbiamo un problema con il buffer di lettura, se usiamo la funzione fgets e' inseriamo un numero
+      di caratteri superiori alla capienza dell'array utilizzando successivamente la funzione scanf esso non ci permetteva di inserire
+      tramite input qualsiasi tipo di carattere poiche' prendeva in considerazione i caratteri in eccesso dal fgets*/
     if(lengthOfString >= sizeLength-1){
-        puts("Entra? Non credo proprio");
         while(getchar() != '\n');
     }
 }
 
-int lengthString(const char string[], const int index){
+int lengthString(const char string[]){
     //Scorriamo tutto l'array e' aggiorniamo la sua lunghezza fino alla sua terminazione (\0)
     int length=0;
 
@@ -149,18 +173,50 @@ int lengthString(const char string[], const int index){
         length++;
     }
 
-    printf("Lunghezza: %d\n", length);
     return length;
 }
 
 void inputString(char stringa[], int index){
-
+    //prendo in input una serie di caratteri con fgets() e successivamente pulisco il buffer input
     fgets(stringa, index, stdin);
-    int lengthOfStringa = lengthString(stringa, index);
+    int lengthOfStringa = lengthString(stringa);
 
-    eliminazioneInput(stringa, index, lengthOfStringa);
+    deleteInput(stringa, index, lengthOfStringa);
 }
 
 void puliziaScan(){
+    //elimino il \n rimasto in input
     getchar();
+}
+
+void repeatKey(char key[], int lengthKey, char newKey[], int lengthNewKey){
+    //inserisco ogni carattere nell'array newKey[], quando la chiave inserita dall'utente sara' terminata riparto dalla posizione 0
+    int j=0;
+    for(int i=0; i<lengthNewKey; i++){
+        newKey[i] = key[j];
+        j++;
+        if(j == lengthKey){
+            j=0;
+        }
+    }
+    newKey[lengthNewKey] = '\0'; //aggiungo il carattere di terminazione per non avere problemi di output, etc... successivamente
+}
+
+void cypherOfText(char stringa[],char key[], char cypherText[], int lengthCypher){
+    /* cifro il testo dato dall'utente utilizzando il cifrario di Vigenere quindi con l'uso di una chiave
+        Funzionamento: sommo i due valori dei caratteri, se la somma e maggiore della somma di 'a'+'z'
+        dobbiamo far tornare il valore ad 'a' quindi sottraiamo il valore di 'z' altrimenti sottraiamo il valore
+        di 'a'.
+    */
+    for(int i=0; i<lengthCypher; i++){
+        int valore = stringa[i] + key[i];
+        if(valore > 219){
+            valore -= 123;
+        }else{
+            valore -= 97;
+        }
+        //printf("%d %c, %d %c, %d %c\n", stringa[i],stringa[i], newKey[i],newKey[i],valore,valore); (debug)
+        cypherText[i] = valore;
+    }
+    cypherText[lengthCypher] = '\0';
 }
